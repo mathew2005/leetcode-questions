58aef92f119b1e508e9073c8ae90bcac0bfa4efb
Mathewos <89460058+mathew2005@users.noreply.github.com>
2025-11-03 19:53:56 -0500

    Time: 11 ms (72.18%) | Memory: 18 MB (49.28%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -2,9 +2,24 @@ class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         # same length
         # sort both s and t, check if s == t
-        s = "".join(sorted(s))
-        t = "".join(sorted(t))
-        return s == t
+        sMap = {}
+        tMap = {}
+        
+        # loop through the string
+        for char in s:
+            if char not in sMap:
+                sMap[char] = 1
+            else:
+                sMap[char] += 1
+
+        for char in t:
+            if char not in tMap:
+                tMap[char] = 1
+            else:
+                tMap[char] += 1
+                
+        return sMap == tMap
+            
         
         # T: O(nlogn + nlogn)
         # S:O(1)
