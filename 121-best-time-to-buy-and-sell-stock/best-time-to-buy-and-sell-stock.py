8c61293baac4f241a08890b7bc626d3231b387d5
Mathewos <89460058+mathew2005@users.noreply.github.com>
2025-11-05 19:39:59 -0500

    Time: 147 ms (8.46%) | Memory: 27 MB (32.91%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -3,10 +3,18 @@ class Solution:
         min_value = prices[0]
         max_profit = 0
 
-        for i in range(len(prices)):
-            if prices[i] < min_value:
-                min_value = prices[i]
-            else:    
-                max_profit = max(max_profit, prices[i] - min_value)
+        for i in range(1, len(prices)):
+            min_value = min(min_value, prices[i-1])
+            
+            max_profit = max(max_profit, prices[i] - min_value)
 
         return max_profit
+
+#    max_clar = 0
+
+#    # loop through prices
+#    for i in range(1,len(prices)):
+#       min_value = min(min_value, prices[i - 1])
+#       curr_clar = prices[i] - min_value
+#       max_clar = max(curr_clar, max_clar)
+#    return max_clar
