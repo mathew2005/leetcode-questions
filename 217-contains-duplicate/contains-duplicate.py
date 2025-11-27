5d899544c979f998a5281afcbc85166efc2cae58
Mathewos <89460058+mathew2005@users.noreply.github.com>
2025-11-27 02:36:31 -0500

    Time: 56 ms (5.37%) | Memory: 26.5 MB (99.48%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.py
+++ 217-contains-duplicate/contains-duplicate.py
@@ -1,12 +1,24 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        # sort the list and have 2 pointers
         
-        freqSet = set()
+        # freqSet = set()
 
-        for num in nums:
-            if num in freqSet:
+        # for num in nums:
+        #     if num in freqSet:
+        #         return True
+        #     else:
+        #         freqSet.add(num)
+        # return False
+
+
+        # sort the list and have 2 pointers
+        nums.sort()
+        prev = 0
+        curr = 1
+        while curr < len(nums):
+            if nums[prev] == nums[curr]:
                 return True
-            else:
-                freqSet.add(num)
+            prev += 1
+            curr += 1
+
         return False
