fda3efefe5157596023e8dae07f4c15917721360
Mathewos <89460058+mathew2005@users.noreply.github.com>
2025-12-03 00:40:59 -0500

    Time: 15 ms (15.60%) | Memory: 19.3 MB (87.02%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -2,5 +2,15 @@ class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         # [2,2,3,3,3,3]
         # [1,1,1,1,2,3,]
-        nums.sort()
-        return nums[len(nums) // 2]
+        freqMap = {}
+        currFreq = 0
+        for num in nums:
+            if num in freqMap:
+                freqMap[num] +=1
+            else:
+                freqMap[num] = 1
+        
+        for num in nums:
+            currFreq = freqMap[num]
+            if currFreq > len(nums)/2:
+                return num
