64a8e4996c780c894edeb14f57613af809b80dca
Mathewos <89460058+mathew2005@users.noreply.github.com>
2026-09-09 09:52:45 -0400

    Time: 523 ms (5.02%) | Memory: 45.4 MB (10.17%) - LeetSync

--- 148-sort-list/sort-list.py
+++ 148-sort-list/sort-list.py
@@ -5,15 +5,46 @@
 #         self.next = next
 class Solution:
     def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        temp = []
-        curr = head
-        while curr:
-            temp.append(curr.val)
-            curr = curr.next
-        temp.sort()
+        if not head or not head.next:
+            return head
+        
+        left = head
+        right = self.getMid(head)
+        tmp = right.next
+        right.next = None
+        right = tmp
+
+        left = self.sortList(left)
+        right = self.sortList(right)
+
+        return self.merge(left,right)
+    def getMid(self,head):
+        slow, fast = head, head.next
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        return slow
+    
+    def merge(self,list1,list2):
         dummy = ListNode()
         curr = dummy
-        for val in temp:
-            curr.next = ListNode(val)
+        while list1 and list2:
+            if list1.val <= list2.val:
+                curr.next = ListNode(list1.val)
+                list1 = list1.next
+            else:
+                curr.next = ListNode(list2.val)
+                list2 = list2.next
+
             curr = curr.next
+        while list1:
+            curr.next = ListNode(list1.val)
+            list1 = list1.next
+            curr = curr.next
+
+        while list2:
+            curr.next = ListNode(list2.val)
+            list2 = list2.next
+            curr = curr.next
+
         return dummy.next
