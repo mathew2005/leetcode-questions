cc1441670be78017897af84466a4be2dfa084b55
Mathewos <89460058+mathew2005@users.noreply.github.com>
2026-09-16 13:49:45 -0400

    Time: 187 ms (29.17%) | Memory: 40.6 MB (87.09%) - LeetSync

--- 148-sort-list/sort-list.py
+++ 148-sort-list/sort-list.py
@@ -4,43 +4,36 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    # MAKE THIS RECURSIVE
-    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        # progressivley half the linked list
-            # find the middle
-            if not head or not head.next: 
-                return head
-            # mid = self.middle(head)
-            right = self.sortList(self.middle(head))
-            left = self.sortList(head)
+    def sortList(self, head: ListNode | None) -> ListNode | None:
+        if not head or not head.next:
+            return head
+        
+        right = self.sortList(self.findMid(head))
+        left = self.sortList(head)
+        
 
-            return self.merge(left, right)
+        return self.merge(left, right)
 
-
-        # sort while merging as we go (when building back up)
-            # sort two linked list parts
-            # merge them together
-
-    def middle(self, head):
-        slowPtr = head
-        fastPtr = head.next
-        while fastPtr and fastPtr.next:
-            slowPtr = slowPtr.next
-            fastPtr = fastPtr.next.next
-        mid = slowPtr.next
-        slowPtr.next = None
-        return mid
-
+    def findMid(self,head):
+        slow = head
+        fast = head.next 
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        tail = slow.next
+        slow.next = None
+        return tail
 
     def merge(self,list1,list2):
-        dummy = tail = ListNode()
+        tail = dummy = ListNode()
         while list1 and list2:
-            if list1.val < list2.val:
+            if list1.val <= list2.val:
                 tail.next = list1
                 list1 = list1.next
             else:
                 tail.next = list2
                 list2 = list2.next
+            
             tail = tail.next
         if list1:
             tail.next = list1
