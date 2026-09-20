a957e899d4d82ffb9a43a2b4396a3cfe802bd87c
Mathewos <89460058+mathew2005@users.noreply.github.com>
2026-09-20 15:10:20 -0400

    Time: 0 ms (100.00%) | Memory: 19.2 MB (71.17%) - LeetSync

---
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.py
@@ -0,0 +1,22 @@
+from collections import deque
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rightSideView(self, root: TreeNode | None) -> list[int]:
+        if not root: return []
+        q = deque([root])
+        res = [root.val]
+        while q:
+            for i in range(len(q)):
+                curr = q.popleft()
+                if curr.left:
+                    q.append(curr.left)
+                if curr.right:
+                    q.append(curr.right)
+            if q:
+                res.append(q[-1].val)
+        return res
